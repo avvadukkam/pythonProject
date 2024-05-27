826ecc611599a6ecb7d1b8b2b75e84bbf5d5cfb8
avvadukkam <davvadukkam@gmail.com>
2024-05-26 21:28:13 -0400

    with 4 questions

--- quiz_game.py
+++ quiz_game.py
@@ -14,3 +14,27 @@ if answer.lower() == "central processing unit":
     score += 1
 else:
     print("Incorrect!")
+
+answer = input("What does GPU stands for? ")
+if answer.lower() == "graphics processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does RAM stands for? ")
+if answer.lower() == "random access memory":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does PSU stands for? ")
+if answer.lower() == "power supply":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+print("You got " + str(score) + " questions correct!")
+print("You got " + str((score/4) * 100) + "%.")
